b7dc886bd44d48df155aed7f57350c5322eedb07
NicolasPreiti <nicolaspreiti45@gmail.com>
2022-09-01 02:38:04 -0300

    fixes with the port

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'http://localhost:3000',
     },
 });
 
@@ -25,7 +25,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
     });
 });
 
