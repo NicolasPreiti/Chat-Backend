239f8c384b3470ac939576afc39ae04fe12177d3
NicolasPreiti <nicolaspreiti45@gmail.com>
2022-09-01 05:53:57 -0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,6 @@ io.on('connection', (socket) => {
         });
     });
 
-    socket.on('disconnect', () => {
-    });
 });
 
 server.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));
